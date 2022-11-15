3fce4ded3bdab271c1b80efda618bc491ad76a60
Leophen <Leophen@foxmail.com>
2022-11-15 14:39:59 +0800

    feat: 接口路由初始化

---
+++ src/router/user.js
@@ -0,0 +1,12 @@
+const handleUserRouter = (req, res) => {
+  const method = req.method
+
+  // 登陆
+  if (method === 'POST' && req.path === '/api/user/login') {
+    return {
+      msg: '用户登陆的接口'
+    }
+  }
+}
+
+module.exports = handleUserRouter
