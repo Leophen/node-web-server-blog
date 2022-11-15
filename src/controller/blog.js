ad68e5a177e9b4e7fdeec71d5f0247dbadf367c1
Leophen <Leophen@foxmail.com>
2022-11-15 15:30:38 +0800

    feat: 获取博客详情接口

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -18,6 +18,18 @@ const getList = (author, keyword) => {
   ]
 }
 
+const getDetail = (id) => {
+  // 暂时返回正确格式的假数据
+  return {
+    id: 1,
+    title: '标题 A',
+    content: '内容 A',
+    createTime: 1668496456053,
+    author: 'leophen'
+  }
+}
+
 module.exports = {
-  getList
+  getList,
+  getDetail
 }
