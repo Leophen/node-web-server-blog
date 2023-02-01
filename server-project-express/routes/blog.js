e6800174b495921993fda2d7349aa14fcea2b5d5
Leophen <Leophen@foxmail.com>
2023-02-01 18:04:09 +0800

    feat: Express 编写登录验证中间件

--- server-project-express/routes/blog.js
+++ server-project-express/routes/blog.js
@@ -12,14 +12,7 @@ const {
   SuccessModel,
   ErrorModel
 } = require('../model/resModel')
-
-// 统一的登录验证函数
-const isLogin = (req) => {
-  if (!req.session.username) {
-    return false
-  }
-  return true
-}
+const loginCheck = require('../middleware/loginCheck')
 
 // 获取博客列表总数
 router.get('/count', (req, res, next) => {
@@ -51,52 +44,40 @@ router.post('/detail', (req, res, next) => {
 });
 
 // 新建一篇博客
-router.post('/new', (req, res, next) => {
-  if (!isLogin(req)) {
-    res.json(new ErrorModel('尚未登录'))
-  } else {
-    const result = newBlog(req.body, req.session.username)
-    return result.then(data => {
-      res.json(new SuccessModel(data))
-    })
-  }
+router.post('/new', loginCheck, (req, res, next) => {
+  const result = newBlog(req.body, req.session.username)
+  return result.then(data => {
+    res.json(new SuccessModel(data))
+  })
 });
 
 // 更新一篇博客
-router.post('/update', (req, res, next) => {
-  if (!isLogin(req)) {
-    res.json(new ErrorModel('尚未登录'))
-  } else {
-    const result = updateBlog(req.body, req.session.username)
-    return result.then(val => {
-      if (val === 0) {
-        res.json(new SuccessModel('编辑成功'))
-      } else if (val === -1) {
-        res.json(new ErrorModel('不能编辑他人的博客'))
-      } else {
-        res.json(new ErrorModel('编辑失败'))
-      }
-    })
-  }
+router.post('/update', loginCheck, (req, res, next) => {
+  const result = updateBlog(req.body, req.session.username)
+  return result.then(val => {
+    if (val === 0) {
+      res.json(new SuccessModel('编辑成功'))
+    } else if (val === -1) {
+      res.json(new ErrorModel('不能编辑他人的博客'))
+    } else {
+      res.json(new ErrorModel('编辑失败'))
+    }
+  })
 });
 
 // 删除一篇博客
-router.post('/del', (req, res, next) => {
-  if (!isLogin(req)) {
-    res.json(new ErrorModel('尚未登录'))
-  } else {
-    const { id } = req.body
-    const result = delBlog(id, req.session.username)
-    return result.then(val => {
-      if (val === 0) {
-        res.json(new SuccessModel('删除成功'))
-      } else if (val === -1) {
-        res.json(new ErrorModel('不能删除他人的博客'))
-      } else {
-        res.json(new ErrorModel('删除失败'))
-      }
-    })
-  }
+router.post('/del', loginCheck, (req, res, next) => {
+  const { id } = req.body
+  const result = delBlog(id, req.session.username)
+  return result.then(val => {
+    if (val === 0) {
+      res.json(new SuccessModel('删除成功'))
+    } else if (val === -1) {
+      res.json(new ErrorModel('不能删除他人的博客'))
+    } else {
+      res.json(new ErrorModel('删除失败'))
+    }
+  })
 });
 
 module.exports = router;
